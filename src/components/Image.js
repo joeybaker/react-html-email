26222f00b4836e3f82539a89bd9a743f358692b1
Joey Baker <joey@byjoeybaker.com>
2016-04-11 11:23:15 -0700

    Change: Image: now responsive by default

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,19 +3,31 @@ import EmailPropTypes from '../PropTypes'
 
 export default function Image(props) {
   return (
-    <img src={props.src} width={props.width} height={props.height} style={{
-      display: 'block',
-      outline: 'none',
-      border: 'none',
-      textDecoration: 'none',
-      ...props.style,
-    }}/>
+    <img
+      src={props.src}
+      srcSet={props.srcSet}
+      width={props.width}
+      height={props.height}
+      style={{
+        display: props.inline ? 'inline': 'block',
+        outline: 'none',
+        border: 'none',
+        textDecoration: 'none',
+        maxWidth: '100vw !important',
+        width: props.width ? `${props.width}px` : '100% !important',
+        // height: props.height && !props.srcSet ? `${props.height}px` : 'auto !important',
+        height: 'auto !important',
+        ...props.style
+      }}
+    />
   )
 }
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
+  srcSet: PropTypes.string,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
   style: EmailPropTypes.style,
+  inline: PropTypes.bool
 }
